1e048f129ba75f110753f7c8ac46ff0b4a6b61ba
BookmDan <danoh329@gmail.com>
2023-09-26 20:58:54 -0700

    what I have so far, but will restart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,24 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onToggleIsInCart, onDelete }) {
+  const handleToggleIsInCart = () => {
+    onToggleIsInCart(item.id)
+  }
+
+  const handleDelete = () => {
+    onDelete(item.id)
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleToggleIsInCart}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDelete}>Delete</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,66 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(null)
+  // const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   const itemData = {
+  //     name: name,
+  //     category: category,
+  //     isInCart: false,
+  //   };
+
+  //   fetch("http://localhost:4000/items", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(itemData),
+  //   })
+  //     .then((r) => r.json())
+  //     .then((newItem) => console.log(newItem));
+  // }
+
+  
+  const fetchItems = () => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => {
+        setItems(items)
+        // setLoading(false)
+      })
+      .catch((e) => {
+        console.error("Error fetching data: ", e)
+        setError("Error fetching data. Please try again later.")
+        // setLoading(false)
+      })
+  }
+
+  function handleToggleIsInCart(itemId) {
+    // Update the isInCart status for the item with the specified itemId
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, isInCart: !item.isInCart } : item
+      )
+    );
+  }
+
+  function handleAddItem(newItem) {
+    // console.log("In ShoppingList:", newItem)
+    setItems([...items, newItem])
+  }
+
+  function handleDeleteItem(itemId) {
+    console.log('Deleting item with ID:', itemId);
+    // Remove the item with the specified itemId from the list
+    setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -13,20 +73,29 @@ function ShoppingList() {
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
+      {error &&<div className="error-message">{error}</div>}
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <div key={item.id}>
+            <Item
+              key={item.id}
+              item={item}
+              onToggleIsInCart={handleToggleIsInCart}
+              onDelete={handleDeleteItem}
+            />
+            {/* <button onClick={() => handleDeleteItem(item.id)}>Delete</button> */}
+          </div>
         ))}
       </ul>
     </div>
